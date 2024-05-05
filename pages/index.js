6ab7fc31c0900228dc99117ce0725d0168448abb
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-05-05 22:56:14 +0330

    creating sidebar

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 
 export default function Home() {
   return (
-    <div>
-      home
+    <div className='home'>
+     <div className="sidebar">
+      <ul>
+        <li>Signin</li>
+        <li>Dashboard</li>
+        <li>Todo</li>
+        <li>Signout</li>
+      </ul>
+     </div>
+     <div>
+      <h1 className='home-title'>Welcome To My Todo App</h1>
+     </div>
     </div>
   );
 }
