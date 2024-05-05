03bcf6c41757e2e99a19692d3cb32cdfdde293b9
saharNrv <122102473+saharNrv@users.noreply.github.com>
2024-05-06 00:04:11 +0330

    creating signup form page

---
+++ pages/signup/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export default function signUp() {
+  return (
+    <div className='signup'>
+      <div className='form-wrap'>
+        <h1 className='form-title'>SignUp Form</h1>
+        <form>
+            <div className="input-box">
+                <input type="text" placeholder='Name' />
+                <label>name</label>
+            </div>
+            <div className="input-box">
+                <input type="text" placeholder='Username' />
+                <label>username</label>
+            </div>
+            <div className="input-box">
+                <input type="text" placeholder='Email' />
+                <label>email</label>
+            </div>
+            <div className="input-box">
+                <input type="password" placeholder='Password' />
+                <label>password</label>
+            </div>
+            <input type="submit" className='register-btn' />
+        </form>
+      </div>
+    </div>
+  );
+}
